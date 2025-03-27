3e9c1ca40f015bd8b5e093f49bec5bfdd69f5424
Nancy Weng <wengalex55@gmail.com>
2025-03-27 09:22:32 -0400

    flashcards 2

--- teacher.py
+++ teacher.py
@@ -1,4 +1,16 @@
 import json
 
+def save_flashcard(flashcards):
+    with open("flashcard.json", "w") as file:
+        json.dump(flashcards, file, indent=4) #saves flashcards to JSON file
 
+def load_flashcard():
+    try:
+        with open("flashcard.json", "r") as file:
+            flashcards = json.load(file)
+    except FileNotFoundError:
+        flashcards = []
+    return flashcards
 
+def teacher_mode():
+    flashcards = load_flashcard()
