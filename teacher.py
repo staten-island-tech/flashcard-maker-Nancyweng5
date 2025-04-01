adb3b36c03ca53df265741e93e4efc6b492c7d22
Nancy Weng <wengalex55@gmail.com>
2025-04-01 09:22:02 -0400

    flashcard 4

--- teacher.py
+++ teacher.py
@@ -9,13 +9,19 @@ def load_flashcard():
         with open("flashcard.json", "r") as file:
             flashcards = json.load(file)
     except FileNotFoundError:
-        flashcards = []
-    return flashcards
+        flashcards = {}
+    return flashcards #load existing data
 
 def teacher_mode():
     flashcards = load_flashcard()
     while True:
-        term = input("Enter a word or phrase : ")
+        term = input("Enter a word or phrase or exit to leave: ")
+        if term.lower() == "exit":
+            break
         definition = input(f"Enter a definition for {term}")
         flashcards[term] = definition
+    save_flashcard(flashcards) 
+    print("Flashcards Saved")
+
+
     
