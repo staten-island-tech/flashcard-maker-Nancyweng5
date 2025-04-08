768ba7b45c460c54c23a23f7e08f362913c2d228
Nancy Weng <wengalex55@gmail.com>
2025-04-08 09:26:19 -0400

    Flashcards 6

--- teacher.py
+++ teacher.py
@@ -2,28 +2,26 @@ import json
 
 def save_flashcard(flashcards):
     with open("flashcard.json", "w") as file:
-        json.dump(flashcards, file, indent=4) #saves flashcards to JSON file
-        print("Flashcards Saved")
+        json.dump(flashcards, file, indent=4)  # saves flashcards to json file
+    print("Flashcards Saved")
 
-    
 def load_flashcard():
     try:
         with open("flashcard.json", "r") as file:
             flashcards = json.load(file)
     except FileNotFoundError:
         flashcards = {}
-    return flashcards #load existing data
+    return flashcards  # return flashcards
 
 def teacher_mode():
     flashcards = load_flashcard()
     while True:
-        term = input("Enter a word or phrase or exit to leave: ")
+        term = input("Enter a word or phrase or 'exit' to leave: ")
         if term.lower() == "exit":
             break
-        definition = input(f"Enter a definition for {term} : ")
+        definition = input(f"Enter a definition for {term}: ")
         flashcards[term] = definition
-    save_flashcard(flashcards) 
-    print("Flashcards Saved")
+    save_flashcard(flashcards)  
     
 teacher_mode()
 
