580ba4d2f9ffb8a369873c97690fed0cfbc17b22
Nancy Weng <wengalex55@gmail.com>
2025-04-21 09:25:11 -0400

    flashcards final

--- student.py
+++ student.py
@@ -8,3 +8,25 @@ def load_flashcard():
     except FileNotFoundError:
         flashcards = {}
     return flashcards
+
+def student_mode():
+    flashcards = load_flashcard()
+
+    score = 0
+    streak = 0 
+
+    for term in flashcards:
+        definition = input(f"what is the definition for {term} : ")
+        if definition.lower() == flashcards[term].lower():
+            print("Correct")
+            streak += 1
+            score += 1
+            if streak >= 3:
+                print("Bonus Points!")
+                score += 1
+        else:
+            print(f"Wrong definition, the correct answer is : {flashcards[term]}")
+            streak = 0
+    print(f"Final Score : {score}")
+
+student_mode()
